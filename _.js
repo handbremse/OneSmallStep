f8a6b19a130350d38fa6a636891cd7ec61cdb1d8
handbremse <tim.gebken@gebken.com>
2020-04-10 21:21:02 +0200

    clean up

--- _.js
+++ _.js
@@ -25,32 +25,32 @@ let handlebarshelpers = require('./helpers/handlebars');
 // database
 const MongoClient = require('mongodb').MongoClient;
 const client = new MongoClient(config.mongo.host, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
 let app = express();
 app.config = config;
 
 client.connect(err => {
-  assert.equal(null, err);
-  console.log("Connected successfully to database");
-  const db = client.db(config.mongo.db);
-  app.db = db;
-  app.use((req, res, next) => {
-    req.app = app;
-    next();
-  });
+    assert.equal(null, err);
+    console.log("Connected successfully to database");
+    const db = client.db(config.mongo.db);
+    app.db = db;
+    app.use((req, res, next) => {
+        req.app = app;
+        next();
+    });
 });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views', 'default'));
 app.engine('hbs', handlebars({
-  extname: 'hbs',
-  layoutsDir: path.join(__dirname, 'views', 'default', 'layouts'),
-  defaultLayout: 'eshop.hbs',
-  partialsDir: [path.join(__dirname, 'views', 'default')],
-  helpers: handlebarshelpers
+    extname: 'hbs',
+    layoutsDir: path.join(__dirname, 'views', 'default', 'layouts'),
+    defaultLayout: 'eshop.hbs',
+    partialsDir: [path.join(__dirname, 'views', 'default')],
+    helpers: handlebarshelpers
 }));
 app.set('view engine', 'hbs');
 
@@ -61,22 +61,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(session({
-  secret: config.session.secret,
-  resave: false,
-  saveUninitialized: true,
-  store: new MongoStore({
-    url: config.mongo.host+'/sessions',
-    ttl: 30 * 24 * 60 * 60 // => first is  days
-  }),
-  cookie: {
-    path: '/',
-    httpOnly: true,
-    secure: false,
-    maxAge: 30 * 24 * 60 * 60 * 1000 // => first is  days
-  },
-  genid: function(req) {
-    return uuidv4()
-  },
+    secret: config.session.secret,
+    resave: false,
+    saveUninitialized: true,
+    store: new MongoStore({
+        url: config.mongo.host+'/sessions',
+        ttl: 30 * 24 * 60 * 60 // => first is  days
+    }),
+    cookie: {
+        path: '/',
+        httpOnly: true,
+        secure: false,
+        maxAge: 30 * 24 * 60 * 60 * 1000 // => first is  days
+    },
+    genid: function(req) {
+        return uuidv4()
+    },
 }));
 app.use(express.static(path.join(__dirname, 'public', 'default')));
 
@@ -99,13 +99,13 @@ app.use('/admin/users', require('./routes/admin/users'));
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 
@@ -115,35 +115,35 @@ server.listen(80);
 
 
 server.on('error', onError => {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
 
-  let bind = typeof port === 'string'
-      ? 'Pipe ' + port
-      : 'Port ' + port;
+    let bind = typeof port === 'string'
+        ? 'Pipe ' + port
+        : 'Port ' + port;
 
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
 });
 
 server.on('listening', () => {
-  let addr = server.address();
-  let bind = typeof addr === 'string'
-      ? 'pipe ' + addr
-      : 'port ' + addr.port;
-  debug('Listening on ' + bind);
+    let addr = server.address();
+    let bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+    debug('Listening on ' + bind);
 });
 
 
